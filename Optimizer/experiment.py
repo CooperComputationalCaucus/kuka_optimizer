9379efdb1d7ca659316da74a381edd9a52fa1a86
Phillip Maffettone <phillip.maffettone@liverpool.ac.uk>
2019-07-28 19:06:01 +0100

    Set up for normal operation

--- Optimizer/experiment.py
+++ Optimizer/experiment.py
@@ -675,15 +675,15 @@ def watch_queue(multiprocessing=1):
 
 
 if __name__ == "__main__":
-#     try:
-#         p1 = multiprocessing.Process(target=watch_completed, args=(360,)) #Delay for model building when finding new data
-#         p1.start()
-#         sleep(Experiment.SLEEP_DELAY)
-#         p2 = multiprocessing.Process(target=watch_queue, args=(12,)) #CPUs used for batch generation
-#         p2.start()
-#     except:
-#         tb = traceback.format_exc()
-#         print(tb)
+    try:
+        p1 = multiprocessing.Process(target=watch_completed, args=(360,)) #Delay for model building when finding new data
+        p1.start()
+        sleep(Experiment.SLEEP_DELAY)
+        p2 = multiprocessing.Process(target=watch_queue, args=(12,)) #CPUs used for batch generation
+        p2.start()
+    except:
+        tb = traceback.format_exc()
+        print(tb)
 
 #     ### DEBUGINING LINES ###
 #     watch_queue(4)
@@ -692,5 +692,5 @@ if __name__ == "__main__":
 #     sleep(Experiment.SLEEP_DELAY)
 #     p2 = multiprocessing.Process(target=watch_queue, args=(4,)) #CPUs used for batch generation
 #     p2.start()
-    watch_queue(10)
+#     watch_queue(10)
 #     ### DEBUGING LINES ###
