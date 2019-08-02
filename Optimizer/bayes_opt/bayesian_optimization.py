92a1426d5fc34863a2daefb7d46338d3891d9cd4
Phillip Maffettone <phillip.maffettone@liverpool.ac.uk>
2019-08-02 16:48:56 +0100

    Fix initial batch sampling in constrained system.
    New configuraiton files

--- Optimizer/bayes_opt/bayesian_optimization.py
+++ Optimizer/bayes_opt/bayesian_optimization.py
@@ -385,7 +385,8 @@ class DiscreteBayesianOptimization(BayesianOptimization):
         list length n_acqs of dictionary style parameters 
         """
         if len(self._space) == 0:
-            return [self._space.array_to_params(self.space._bin(self._space.random_sample(constraints=self.get_constraint_dict()))) for _ in range(kwargs.get('n_acqs',1))]
+            return [self._space.array_to_params(x) for x in self.constrained_rng(kwargs.get('n_acqs',1), bin=True)]
+            #return [self._space.array_to_params(self.space._bin(self._space.random_sample(constraints=self.get_constraint_dict()))) for _ in range(kwargs.get('n_acqs',1))]
 
         # Sklearn's GP throws a large number of warnings at times, but
         # we don't really need to see them here.
