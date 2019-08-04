e9f37c362def409908363851a7bf8081850ed0bc
maffettone <phillip.maffettone@liverpool.ac.uk>
2019-08-04 14:30:41 +0100

    Adjust regex AGAIN to work with arbitrary complement ordering in controls

--- Optimizer/bayes_opt/bayesian_optimization.py
+++ Optimizer/bayes_opt/bayesian_optimization.py
@@ -343,19 +343,17 @@ class DiscreteBayesianOptimization(BayesianOptimization):
                     for complement in complements:
                         x[i,complement] = random_state.uniform(bounds[complement, 0], bounds[complement, 1])
                         # Extract regex, includes two options for ordering issues
-                        try:
-                            p = re.compile(
-                                '- \(\(x\[{:d}\]<0.5\) \* \(\(\(0.5 - x\[{:d}\]\)/0.5\) \* \(\d+.\d+-\d+.\d+\) \+ \d+.\d+\) \) - '
-                                '\(\(x\[{:d}+\]>=0.5\) \* \(\(\(x\[{:d}\] - 0.5\)/0.5\) \* \(\d+.\d+-\d+.\d+\) \+ \d+.\d+\) \)'.format(
-                                    complement,complement,complement,complement))
-                            reduction = p.findall(s)[0]
-                        except IndexError:
-                            p = re.compile(
-                                '- \(\(x\[{:d}+\]>=0.5\) \* \(\(\(x\[{:d}\] - 0.5\)/0.5\) \* \(\d+.\d+-\d+.\d+\) \+ \d+.\d+\) \) - '
-                                '\(\(x\[{:d}\]<0.5\) \* \(\(\(0.5 - x\[{:d}\]\)/0.5\) \* \(\d+.\d+-\d+.\d+\) \+ \d+.\d+\) \)'.format(
-                                    complement, complement, complement, complement))
-                            reduction = p.findall(s)[0]
-                        rem_max_val += pd.eval(reduction,local_dict={'x':x[i,:]})
+                        reductions = []
+                        p = re.compile(
+                            '- \(\(x\[{:d}\]<0.5\) \* \(\(\(0.5 - x\[{:d}\]\)/0.5\) \* \(\d+.\d+-\d+.\d+\) \+ \d+.\d+\) \) '.format(
+                                complement,complement))
+                        reductions.append(p.findall(s)[0])
+                        p = re.compile(
+                            '- \(\(x\[{:d}+\]>=0.5\) \* \(\(\(x\[{:d}\] - 0.5\)/0.5\) \* \(\d+.\d+-\d+.\d+\) \+ \d+.\d+\) \) '.format(
+                                complement, complement))
+                        reductions.append(p.findall(s)[0])
+                        for reduction in reductions:
+                            rem_max_val += pd.eval(reduction,local_dict={'x':x[i,:]})
                 rnd = get_rnd_quantities(rem_max_val,n_constrained_var,random_state)
                 cnt = 0
                 for j in range(n_var): 
